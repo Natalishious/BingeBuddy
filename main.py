bb392577d8e55444a929b4e51b327eed8aefc809
Jimmy <jimmylehti@hotmail.com>
2026-05-10 18:06:15 +0200

    Added easy login/register and db

--- main.py
+++ main.py
@@ -1,23 +1,65 @@
-from flask import Flask, render_template
-from faker import Faker
+from flask import Flask, redirect, render_template, request, url_for
+from database import init_db, create_user, get_user
 from movies import movies, genras, rating
 
+init_db()  # Skapar db
 
+# Lägger till användare:
+success = create_user("Jimpan", "test123")
+print(f"User created: {success}!")
 
-
+# Hämta användare
+user = get_user("Jimpan")
+print(f"User retrieved: {user}")
 
 x = movies()
 y = genras()
 o = rating()
 
 
-
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template("home/home.html", x=x, y=y,o=o)
+    return render_template("home/home.html", x=x, y=y, o=o)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"].strip()
+
+        if not username or not password:
+            return "Fyll i username och password!"
+
+        created = create_user(username, password)
+        if created:
+            # Går tillbaka till home och skapad
+            return redirect(url_for("home"))
+        else:
+            return "Username doesn not exist!"
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"].strip()
+        password = request.form["password"].strip()
+
+        # Hämtar user från databasen
+        user = get_user(username)
+
+        # user finns och lösenord matchar
+        if user and user["password"] == password:
+            return "Inloggning lyckades"
+
+        return "Fel username eller password"
+
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
